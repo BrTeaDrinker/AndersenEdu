b6214929b9d714f0b50f614af4393df2648edaf8
BrTeaDrinker <Burmistr1994@gmail.com>
2021-01-19 17:06:27 +0300

    calculate Factorial

---
+++ src/main/java/Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите целое число для расчета факториала");
+        int number = in.nextInt();
+        int i=number;
+        int factorial=1;
+        while (i!=0){
+            factorial *=i;
+            i--;
+        }
+        System.out.println("Факториал числа " + number + " - число "+ factorial);
+    }
+
+}

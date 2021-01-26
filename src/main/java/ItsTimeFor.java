5b98f1a24d4b0227698b27505e8768cfad6c4b4e
BrTeaDrinker <Burmistr1994@gmail.com>
2021-01-26 13:04:21 +0300

    itsTimeFor program

---
+++ src/main/java/ItsTimeFor.java
@@ -0,0 +1,28 @@
+public class ItsTimeFor {
+    double time, money;
+    String timeFor;
+    public void setTime(double time){
+        this.time = time;
+    }
+    public void setMoney(double money){
+        this.money = money;
+    }
+
+    public void ItsTimeForWhat(){
+        if (time >= 22.0){
+            timeFor = "Время идти спать!";
+        } else if (time >= 19.0 & time < 22.0) {
+            timeFor = "Время смотреть телевизор!";
+        } else if (time > 12.0 & time < 19.0 & money < 50.0){
+            timeFor = "Время идти к соседу!";
+        } else if (time > 12.0 & time < 19.0 & money >= 50.0){
+            timeFor = "Время идти в кафе!";
+        } else if (time >= 8.0 & time <= 12.0 & money >= 10.0){
+            timeFor = "Время идти в магазин!";
+        } else {
+            timeFor = "Не, ну тут я хз, честно";
+        }
+        System.out.println(timeFor);
+    }
+
+}
---
+++ src/main/java/ItsTimeForExecution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ItsTimeForExecution {
+    public static void main(String[] args) {
+        ItsTimeFor what = new ItsTimeFor();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите вермя суток");
+        what.setTime(in.nextDouble());
+        System.out.println("Введите количество денег в кармане");
+        what.setMoney(in.nextDouble());
+        what.ItsTimeForWhat();
+    }
+}
